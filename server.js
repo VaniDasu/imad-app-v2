48bf92bd6a25116470bcaea0aa6415e159424589
VaniDasu <vani2128@gmail.com>
2017-03-02 23:44:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
-    //articleName=articel-Onear 
+    //articleName=article-One 
     var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
