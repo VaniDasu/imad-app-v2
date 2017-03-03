441701d5d3fbd1740f73bcb1cf14886d11551b86
VaniDasu <vani2128@gmail.com>
2017-03-03 14:10:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ element.innerHTML='New Value';
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-    marginLeft+=1;
+    marginLeft=marginLef+1;
     img.style.marginLeft=marginLeft+'px';
 }
 img.onclick = function(){
