6f2034968b02fceadbf5062b8a7231a9a88fd090
VaniDasu <vani2128@gmail.com>
2017-03-06 16:54:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ app.get('/articles/:articleName', function (req, res) {
     //articleName=article-One 
     var articleName=req.params.articleName;
     //"SELECT * FROM article WHERE title= '"
-    pool.query("SELECT * FROM article WHERE title= '" + req.params.articleName+"'", function(err, result){
+    pool.query("SELECT * FROM article WHERE title= $1", req.params.articleName, function(err, result){
         if(err){
             res.status(500).send(err.toString());
         }else{
